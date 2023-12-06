33f26d5a098a241ac552820430e2ac0ef04f4e98
GRVLigiardi <111814761+GRVLigiardi@users.noreply.github.com>
2023-12-06 15:23:18 -0300

    Ajoutez des fichiers projet.

---
+++ CreationQRCode/Controllers/QrController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Drawing;
+using System.IO;
+
+namespace CreationQRCode.Controllers
+{
+    public class QrController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Generate(string text)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            QRCoder.QRCode qrCode = new QRCoder.QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            using (var stream = new MemoryStream())
+            {
+                qrCodeImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return File(stream.ToArray(), "image/png");
+            }
+        }
+    }
+}
